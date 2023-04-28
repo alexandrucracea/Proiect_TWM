32c6e4ed4730c68e320705d5ae24b9b34a74b5ac
Alexandru Cracea <alexandru.cracea29@gmail.com>
2023-04-28 21:22:13 +0300

    [Feature1]Api REST V1.2

--- Proiect_TWM/MainPage.xaml.cs
+++ Proiect_TWM/MainPage.xaml.cs
@@ -14,23 +14,23 @@ public partial class MainPage : ContentPage
 	{
 		_environmentConfiguration = new EnvironmentConfiguration(new ConfigurationBuilder().AddJsonFile("appsettings.json", optional: false, reloadOnChange: true).Build());
 		var key = _environmentConfiguration.GetKeys("Keys","Count");
-		InitializeComponent();
+		//InitializeComponent();
 
 //		26.04.2023
 		ApiHelper.InitializeClient();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+	//private void OnCounterClicked(object sender, EventArgs e)
+	//{
+	//	count++;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+	//	if (count == 1)
+	//		CounterBtn.Text = $"Clicked {count} time";
+	//	else
+	//		CounterBtn.Text = $"Clicked {count} times";
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+	//	SemanticScreenReader.Announce(CounterBtn.Text);
+	//}
 	public async Task LoadPlants()
 	{
 		var plants = await PlantProcessor.LoadPlants();
@@ -38,7 +38,7 @@ public partial class MainPage : ContentPage
 
     private async void ContentPage_Loaded(object sender, EventArgs e)
     {
-		await LoadPlants();
-    }
+		//await LoadPlants();
+	}
 }
 
