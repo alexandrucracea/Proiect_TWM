bc91ad2044a0609af814f1814875cb5e94aaf9d3
Darius DInu <darius_dinu@yahoo.com>
2023-05-07 19:48:15 +0300

    Adaugare text area de test V1.0

---
+++ Proiect_TWM/ViewModel/LoginViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Proiect_TWM.ViewModel
+{
+    public class LoginViewModel
+    {
+        public string Username { get; set; }
+        public ICommand LoginCommand { get; set; }
+
+        public LoginViewModel()
+        {
+            LoginCommand = new Command(async () => await OnLoginAsync());
+        }
+        async Task OnLoginAsync()
+        {
+            var query = new Dictionary<string, object>();
+            query["Username"] = Username;
+        }
+    }
+}
