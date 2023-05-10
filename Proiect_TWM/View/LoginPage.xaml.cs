06d2c54fbf204f4da8227eedb9984e3a781dadd8
Darius DInu <darius_dinu@yahoo.com>
2023-05-10 17:00:11 +0300

    Feature-02---Finish-login-page-view

--- Proiect_TWM/View/LoginPage.xaml.cs
+++ Proiect_TWM/View/LoginPage.xaml.cs
@@ -1,9 +1,18 @@
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.StyleSheets;
+using static System.Net.Mime.MediaTypeNames;
+
 namespace Proiect_TWM.View;
 
 public partial class LoginPage : ContentPage
 {
-	public LoginPage()
-	{
+    public LoginPage()
+    {
 		InitializeComponent();
+
+		using (var reader = new StringReader("^contentpage{background-color: red; text - align: center;}"))
+		{
+			this.Resources.Add(StyleSheet.FromReader(reader));
+		}
 	}
 }
