e2b278351777eda0ef21980595e1fbd5510f589c
Alexandru Cracea <alexandru.cracea29@gmail.com>
2023-06-17 15:07:39 +0300

    Updatesv2

--- Proiect_TWM/MauiProgram.cs
+++ Proiect_TWM/MauiProgram.cs
@@ -19,7 +19,10 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+				fonts.AddFont("Nunito-Regular.ttf","Nunito-Regular");
+				fonts.AddFont("Nunito-SemiBold.ttf","Nunito-Semibold");
+				fonts.AddFont("Nunito-Bold.ttf","Nunito-Bold");
+            });
 
 #if DEBUG
 		builder.Logging.AddDebug();
