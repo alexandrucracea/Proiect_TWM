a94c534ee18f0ceee1679885092064fac1c2fa9e
Alexandru Cracea <alexandru.cracea29@gmail.com>
2023-06-17 21:08:06 +0300

    Styling V1

--- Proiect_TWM/App.xaml.cs
+++ Proiect_TWM/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace Proiect_TWM;
+﻿using Proiect_TWM.View;
+
+namespace Proiect_TWM;
 
 public partial class App : Application
 {
@@ -7,6 +9,12 @@ public partial class App : Application
 		InitializeComponent();
 
 		//MainPage = new AppShell();
-		MainPage = new NavigationPage(new AppShell());
-	}
+		MainPage = new NavigationPage(new HomePageView());
+		//var background = Application.Current.Resources["BACKGROUND"] as Color;
+		//var text = Application.Current.Resources["TEXT"] as Color;
+		//var mainColor = Application.Current.Resources["MAIN"] as Color;
+		//var lightAccent	= Application.Current.Resources["LIGHT-ACCENT"] as Color;
+		//var darkAccent = Application.Current.Resources["DARK-ACCENT"] as Color;
+
+    }
 }
